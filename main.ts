6abd14acfbfa29bc1a4fd39d3302f8a0b66c7650
GUILLERMO2966 <71560170+GUILLERMO2966@users.noreply.github.com>
2020-10-08 15:55:52 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let valor_dado = 0
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        valor_dado = randint(1, 6)
+    }
+    basic.showNumber(valor_dado)
+    basic.clearScreen()
+})
